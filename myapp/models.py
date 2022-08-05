e2cc87211fafc6cde25a12e812ff1519dc5a0494
Abdulrahman Alazwari <aazwari@gmail.com>
2022-08-05 10:13:59 +0300

    update slug

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,7 @@ class Flower(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField(blank=True, default='')
+    slug = models.SlugField(blank=True, default='', unique=True)
 
     def __str__(self):
         return self.title
